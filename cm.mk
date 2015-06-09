40ef328378631d0da8f88cb698c899e6984e7bbe
Kra1o5 <kra1o5x@gmail.com>
2015-06-09 13:03:02 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/bq/vegetahd/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := vegetahd
+PRODUCT_NAME := cm_vegetahd
+PRODUCT_BRAND := bq
+PRODUCT_MANUFACTURER := bq
+PRODUCT_MODEL := Aquaris E5 HD
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+# Run device patches on the tree
+sh device/bq/vegetahd/patches/apply.sh
+
+add_lunch_combo cm_vegetahd-eng
